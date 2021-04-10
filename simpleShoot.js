9c30fb17e1be61316dcbf1b8d5fc22dbabdb2131
Tatarin9 <tatarin9@gmail.com>
2021-04-10 14:00:40 +0300

    second commit simpleshoot

--- simpleShoot.js
+++ simpleShoot.js
@@ -40,33 +40,51 @@ function laserUp (shoot) {
     // }   
 }
 
-function addEnemy () {
-    const playBoard = document.getElementById('play-board');
-    for (let i=1; i <= 1; i++) {
-        setInterval(() => {
-            const enemy = document.createElement('div');
-            playBoard.appendChild(enemy);
-            enemy.className = 'enemy';
-            enemy.style.left = parseInt((Math.random() * 1450)) + 'px';   
-            enemy.style.top = 0 + 'px'; 
-        }, 2000);
-    }
-    for (let i=1; i<10; i++) {
-        setInterval(() => {
-            const enemy = document.createElement('div');
-            enemy.style.top = 0 + 'px'; 
-            let upNum = enemy.style.top;
-            upNum += 5;
-            enemy.style.up = upNum + 'px'; 
-        }, 500);
-    }        
-}  
-      // for (i=1; i<=1; i++) {
-        //     const enemy = document.createElement('div');
-        //     let upNum = enemy.style.top;
-        //     upNum += 5;
-        //     enemy.style.up = upNum + 'px';
-        // }
+function addEnemy() {
+    setInterval(() => {
+        const playBoard = document.getElementById('play-board');
+    const enemy = document.createElement('div');
+    playBoard.appendChild(enemy);
+    enemy.className = 'enemy';
+    enemy.style.left = parseInt((Math.random() * 1450)) + 'px';   
+    enemy.style.top = 0 + 'px'; 
+    enemyDown(enemy);   
 
+    }, 2000);
+       
+}
+
+function enemyDown(enemy) {
+    const roof = enemy.style.top;
+    let roofNum = parseInt(roof.substring(0, roof.length - 2));
+    for (let i=1; i<20; i++) {
+        setInterval (() => {
+            roofNum = roofNum + 2;
+            enemy.style.top = roofNum + 'px';
+        }, 2000); 
+    }      
+}
+
+// function addEnemy () {
+//     const playBoard = document.getElementById('play-board');
+//     for (let i=1; i <= 1; i++) {
+//         setInterval(() => {
+//             const enemy = document.createElement('div');
+//             playBoard.appendChild(enemy);
+//             enemy.className = 'enemy';
+//             enemy.style.left = parseInt((Math.random() * 1450)) + 'px';   
+//             enemy.style.top = 0 + 'px'; 
+//         }, 2000);
+//     }
+//     for (let i=1; i<10; i++) {
+//         setInterval(() => {
+//             const enemy = document.createElement('div');
+//             enemy.style.top = 0 + 'px'; 
+//             let upNum = enemy.style.top;
+//             upNum += 5;
+//             enemy.style.up = upNum + 'px'; 
+//         }, 500);
+//     }        
+// }  
 addEnemy();
 
